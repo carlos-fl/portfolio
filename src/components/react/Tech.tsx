c260f7f24f75ce6c320f9824f7cc4fdeeb2f8d4c
carlos <carlosftru@gmail.com>
2025-06-14 00:37:36 -0600

    Fixed navbar links block behaviour

--- src/components/react/Tech.tsx
+++ src/components/react/Tech.tsx
@@ -4,7 +4,7 @@ export function Tech({ techName }: { techName: string }) {
   return (
     <div className="flex items-center sm:m-5">
       <div>
-        <i className="fa-solid fa-caret-right text-[var(--title-color)] sm:text-5xl text-xl m-1"></i>
+        <i className="fa-solid fa-caret-right text-[var(--title-color)] sm:text-4xl text-xl m-1"></i>
       </div>
       <div>
         <h3 className="text-[var(--text-color)] font-text sm:text-2xl text-base">{ techName }</h3>
