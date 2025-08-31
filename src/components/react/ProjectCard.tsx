c549441947e114007ba4ad7c6391b4537f9e5dc2
carlos <carlosftru@gmail.com>
2025-08-30 20:16:48 -0600

    fixed responsive

--- src/components/react/ProjectCard.tsx
+++ src/components/react/ProjectCard.tsx
@@ -87,8 +87,8 @@ export function ProjectCard({ projects }: { projects: Array<Project> }) {
       {
         !isFocused &&
         projects.map((project: Project) => (
-          <div onClick={ (event) => { WINDOW_WIDTH >= MAX_WIDTH && showDescription(event, project) } } className="flex flex-col w-md h-auto cursor-zoom-in hover:scale-105 transition-transform duration-300 bg-[var(--card-color)] mb-2 mx-3 rounded-lg shadow-sm">
-            <div className="flex flex-col justify-start rounded-t-lg h-64 overflow-hidden">
+          <div onClick={ (event) => { WINDOW_WIDTH >= MAX_WIDTH && showDescription(event, project) } } className="flex flex-col h-auto cursor-zoom-in hover:scale-105 transition-transform duration-300 bg-[var(--card-color)] mb-2 mx-3 rounded-lg shadow-sm">
+            <div className="flex flex-col justify-start rounded-t-lg overflow-hidden">
               <img className="object-contain w-full h-auto" src={ project.image } alt={ project.title } />
             </div>
 
